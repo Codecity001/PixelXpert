b36e02e782821655b7c0e1998f878300418127de
GitHub <pixelxpert@siava.sh>
2025-03-06 04:36:21 +0000

    Version update: Release

--- MagiskChangelog.md
+++ MagiskChangelog.md
@@ -1,3 +1,13 @@
+**4.2.0**  
+- Compatible with all A15 versions  
+- Compatible with A16 beta 1 & 2  
+
+- Added: Extended power menu  
+- Added: Statusbar icons customization  
+   
+- Bug fixes  
+  
+  
 **4.1.1**  
   
 - Bug fixes  
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
 		applicationId = "sh.siava.pixelxpert"
 		minSdk = 33
 		targetSdk = 34
-		versionCode = 436
-		versionName = "canary-437"
+		versionCode = 437
+		versionName = "4.2.0"
 		setProperty("archivesBaseName", "PixelXpert.apk")
 		ndk {
 			//noinspection ChromeOsAbiSupport
