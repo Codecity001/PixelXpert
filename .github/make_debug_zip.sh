9e7b14abe7b1f697a8ac29d1be4ec331f158d1bd
Codecity001 <sanganiharish263@gmail.com>
2026-06-22 13:27:01 -0500

    ci: add workflow pipelines for automated testing and debug package generation
    Change-Id: Ic7274a2fb39f366a1b2da9a23807e53139ea5e0b

--- .github/make_debug_zip.sh
+++ .github/make_debug_zip.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-mkdir output
-cp app/build/outputs/apk/debug/PixelXpert-signed.apk MagiskModBase/system/priv-app/PixelXpert/PixelXpert.apk
+mkdir -p output
+if [ -f app/build/outputs/apk/debug/PixelXpert-signed.apk ]; then
+  cp app/build/outputs/apk/debug/PixelXpert-signed.apk MagiskModBase/system/priv-app/PixelXpert/PixelXpert.apk
+else
+  cp app/build/outputs/apk/debug/PixelXpert.apk MagiskModBase/system/priv-app/PixelXpert/PixelXpert.apk
+fi
 cd MagiskModBase
 
 FILENAME="PixelXpert.zip"
