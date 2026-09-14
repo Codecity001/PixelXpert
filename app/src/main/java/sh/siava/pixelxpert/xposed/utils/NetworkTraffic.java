2ce5d9aea8bd395fa5c4a274191811eb8764b876
Anirudh Gupta <anirudhgupta109@gmail.com>
2026-09-15 01:25:05 +0530

    CHANGELOG: fix(NetworkTraffic): prevent handler freeze and massive speed spikes
    - Removed the redundant `timeDelta < refreshInterval` check.
    Previously, if refresh interval is increased, this check would cause the handler to return early, breaking the UI update loop, freezing the network traffic indicator.
    
    - Added `clearHandlerCallbacks()` inside `update()` before queuing a new update message.
    This prevents a race condition where a pending handler message could fire immediately after timers were reset, causing the app to calculate speed over a tiny time delta and display massive, fake speed spikes (e.g., 750 KB/s) while idle.
    
    This change is a modified version of https://github.com/Codecity001/PixelXpert/commit/f40801bbfbc8f7af2b64dd014302dbf8a500fd54
    
    Change-Id: I48040739863e35defbb58ce9d17f4a91c50005fc
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Co-authored-by: Codecity001 <sanganiharish263@gmail.com>
    Signed-off-by: Codecity001 <sanganiharish263@gmail.com>

--- app/src/main/java/sh/siava/pixelxpert/xposed/utils/NetworkTraffic.java
+++ app/src/main/java/sh/siava/pixelxpert/xposed/utils/NetworkTraffic.java
@@ -109,12 +109,6 @@ public class NetworkTraffic extends FrameLayout {
 		public void handleMessage(@NonNull Message msg) {
 			long timeDelta = SystemClock.elapsedRealtime() - lastUpdateTime;
 
-			if (timeDelta < refreshInterval * 1000L - 500L) {
-				if (msg.what != 1) {
-					// we just updated the view, nothing further to do
-					return;
-				}
-			}
 			if (timeDelta < 1) {
 				// Can't div by 0 so make sure the value displayed is minimal
 				timeDelta = Long.MAX_VALUE;
@@ -345,6 +339,7 @@ public class NetworkTraffic extends FrameLayout {
 		if (mAttached) {
 			totalRxBytes = TrafficStats.getTotalRxBytes();
 			lastUpdateTime = SystemClock.elapsedRealtime();
+			clearHandlerCallbacks();
 			mTrafficHandler.sendEmptyMessage(1);
 		}
 	}
